03d3c1d52e8e1aba60c932ab7d99d07cc8f02130
KurdyukovAl <leha.kurdyukov.99@mail.ru>
2025-12-05 00:12:12 +0300

    добавление конэкшнов в бд

--- cmd/main.go
+++ cmd/main.go
@@ -2,22 +2,44 @@ package main
 
 import (
 	"log"
+	"os"
 	"todo"
 	"todo/pkg/handler"
 	"todo/pkg/repository"
 	"todo/pkg/service"
 
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 	"github.com/spf13/viper"
 )
 
 func main() {
 	if err := initConfig(); err != nil {
-		log.Fatal("error while initialize config")
+		log.Fatalf("error while initialize config: %s", err.Error())
 	}
+
+	if err := godotenv.Load(); err != nil {
+		log.Fatal("error loading env variables: %s", err)
+	}
+
+	db, err := repository.NewPostgresDB(repository.Config{
+		Host:     viper.GetString("db.host"),
+		Port:     viper.GetString("db.port"),
+		Username: viper.GetString("db.username"),
+		Password: viper.GetString(os.Getenv("DB_PASSWORD")),
+		DBname:   viper.GetString("db.dbname"),
+		SSLMode:  viper.GetString("db.sslmode"),
+	})
+
+	if err != nil {
+		log.Fatalf("could not connect to DB: %s", err)
+	}
+
+	defer db.Close()
 
 	port := viper.GetString("port")
 
-	repos := repository.NewRepository()
+	repos := repository.NewRepository(db)
 	services := service.NewService(repos)
 	handlers := handler.NewHandler(services)
 
@@ -30,7 +52,7 @@ func main() {
 }
 
 func initConfig() error {
-	viper.AddConfigPath("congfigs")
+	viper.AddConfigPath("configs")
 	viper.SetConfigName("config")
 
 	return viper.ReadInConfig()
